5f447eef31ff3a8834c25e372f970f4002967169
Andac Tan <andac.tan@gmail.com>
2020-06-24 12:43:20 +0200

    refactored index page

--- react-tutorial-material/src/pages/index2.js
+++ react-tutorial-material/src/pages/index2.js
@@ -7,7 +7,7 @@ import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import Navbar from '../components/navbar';
 import BlogCard from '../components/blog-card';
 import ForumCard from '../components/forum-card';
-import { Typography, Paper, Divider, Button} from '@material-ui/core';
+import { Typography, Paper, Divider, Button, Grid} from '@material-ui/core';
 import CountUp from 'react-countup';
 import StatisticCard from '../components/statistic-card';
 
@@ -16,124 +16,144 @@ export default class Index2 extends React.Component {
     return (
         <div style={{backgroundColor: "#FFFFFF"}}>
             <Navbar></Navbar>
-            <div class="wrapper" style={{display: "flex", flexDirection: "row", justifyContent: "center", "alignItems": "center", paddingTop: "130px", width:"70%", margin: "0 auto"}}>
-              <div class="wrapper" style={{paddingTop: "30px"}}> {/** left div */}
+
+            <Grid container spacing={2} direction="row" justify="center" alignItems="center" style={{width: "75%", margin: "0 auto"}}>
+              <Grid item xs={12} sm={7} style={{padding: "0"}}>
+                <Grid container spacing={2} direction="column" justify="center" alignItems="flex-start">
+                  <Grid item>
                     <Typography variant="h3" align="left" style={{fontWeight: "bold"}}>
-                        Welcome to CommonRoad
+                      Welcome to CommonRoad
                     </Typography>
+                  </Grid>
 
-                    <div style={{display: "flex", justifyContent: "left" , alignItems: "left", paddingTop: "1%"}}>
-                        <div id="welcome-text" style={{width: "70%"}}>
-                            <Typography align="left" style={{fontWeight: "500"}}>
-                                CommonRoad is a collection of composable benchmarks for motion planning on roads.
-                                This website presents our benchmark collection and provides researchers with a means of evaluating and
-                                comparing their motion planners. For further information, read up on the introduction below.
-                                Also, have a look at our scenarios.
-                            </Typography>
-                        </div>
-                    </div>
-                </div>
+                  <Grid item>
+                    <Typography align="left" style={{fontWeight: "500"}}>
+                      CommonRoad is a collection of composable benchmarks for motion planning on roads.
+                      This website presents our benchmark collection and provides researchers with a means of evaluating and
+                      comparing their motion planners. For further information, read up on the introduction below.
+                      Also, have a look at our scenarios.
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
 
-                <div style={{width:"100%", paddingTop: "20px"}}>
-                  <img  style={{width:"90%"}} src={require("../assets/static_img_cr_flowchart.svg")} />
-                </div>
-                <Divider style={{marginTop: "40px"}}></Divider>
+              <Grid item xs={12} sm={5} style={{padding: "0"}}>
+                <img  style={{width:"90%"}} src={require("../assets/static_img_cr_flowchart.svg")} />
+              </Grid>
+            </Grid> {/** welcome container */}
 
-            </div>
+            <Divider style={{marginTop: "40px"}}></Divider>
+            
+            <Grid container spacing={2} direction="column" justify="center" alignItems="center" style={{width: "75%", margin: "0 auto"}}>
+              <Grid item xs={12} style={{padding:"0"}}>
+                <Typography variant="h3" align="left" style={{fontWeight: "bold"}}>
+                    Scenarios and Benchmarks
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Grid container spacing={1} direction="row" justify="center" alignItems="center">
+                  <Grid item xs={12} sm={6}>
+                    <Grid container spacing={1} direction="row">
+                      <Grid item sm={12} md={4}>
+                        <StatisticCard text="active users"></StatisticCard>
+                      </Grid>
+                      <Grid item sm={12} md={4}>
+                        <StatisticCard text="scenarios"></StatisticCard>
+                      </Grid>
+                      <Grid item sm={12} md={4}>
+                        <StatisticCard text="submissions"></StatisticCard>
+                      </Grid>
+                    </Grid> 
+                  </Grid>{/** statistic cards */}
 
-            <div class="wrapper">
+                  <Grid item xs={12} sm={6}>
+                    <img style={{width:"100%"}} src={require("../assets/scenario.gif")} />  
+                  </Grid> {/** some image */}
+                </Grid>
+              </Grid>
+            </Grid> {/** scenarios and benchmarks */}
 
-              <div style={{width:"70%", margin: "0 auto", paddingTop: "100px"}}>
-              <Divider style={{marginTop: "40px"}}></Divider>
-                  <div class="wrapper" style={{paddingTop: "30px"}}> {/** left div */}
-                    <div>
-                        <Typography variant="h3" align="left" style={{fontWeight: "bold"}}>
-                            Scenarios and Benchmarks
-                        </Typography>
+            <Divider style={{marginTop: "40px"}}></Divider>
 
-                        <div style={{display: "flex", justifyContent: "left" , alignItems: "left", paddingTop: "1%"}}>
-                        <div style={{width:"100%", display: "flex", marginTop: "20px"}}>
-                          <StatisticCard style={{width:"10%"}} text="active users"></StatisticCard>
-                          <StatisticCard style={{width:"10%"}} text="scenarios"></StatisticCard>
-                          <StatisticCard style={{width:"10%"}} text="submissions"></StatisticCard>
-                        </div>
-                            <div style={{width:"50%", margin: "0 auto", paddingTop: "0px"}}>
-                              <img style={{width:"100%"}} src={require("../assets/scenario.gif")} />
-                            </div>
-                        </div>
+            <Grid container spacing={2} direction="column" justify="center" alignItems="center" style={{width: "75%", margin: "0 auto"}}>
+              <Grid item xs={12}>
+                <Typography variant="h3" align="left" style={{fontWeight: "bold"}}>
+                  Tools"12  qWA<
+                </Typography>
+              </Grid>
 
-                    </div>
+              <Grid item>
+                <Grid container spacing={3} direction="row" justify="center" alignItems="center">
+                  <Grid item sm={12} md={3}>
+                    <BlogCard text="This is a blog post header"></BlogCard>
+                  </Grid>
+                  <Grid item sm={12} md={3}>
+                    <BlogCard text="This is a blog post header"></BlogCard>
+                  </Grid>
+                  <Grid item sm={12} md={3}>
+                    <BlogCard text="This is a blog post header"></BlogCard>
+                  </Grid>
+                  <Grid item sm={12} md={3}>
+                    <BlogCard text="This is a blog post header"></BlogCard>
+                  </Grid>
+                </Grid>
+              </Grid>
 
-                  </div>
+              <Grid item>
+                <Button variant="outlined" style={{outline: "none", background: "#1D3557", color: "white"}}>View More</Button>
+              </Grid>
+            </Grid> {/** tools */}
 
+            <Divider style={{marginTop: "40px"}}></Divider>
 
-                  <div style={{display: "flex", flexDirection: "row", justifyContent: "center" , alignItems: "center", paddingTop: "1%"}}>
+            <Grid container spacing={2} direction="column" justify="center" alignItems="center" style={{width: "75%", margin: "0 auto"}}>
+              <Grid item xs={12}>
+                <Typography variant="h3" align="left" style={{fontWeight: "bold"}}>
+                  News
+                </Typography>
+              </Grid>
 
-                  </div>
+              <Grid item>
+                <Grid container spacing={3} direction="row" justify="center" alignItems="center">
+                  <Grid item sm={12} md={3}>
+                    <BlogCard text="This is a blog post header"></BlogCard>
+                  </Grid>
+                  <Grid item sm={12} md={3}>
+                    <BlogCard text="This is a blog post header"></BlogCard>
+                  </Grid>
+                  <Grid item sm={12} md={3}>
+                    <BlogCard text="This is a blog post header"></BlogCard>
+                  </Grid>
+                  <Grid item sm={12} md={3}>
+                    <BlogCard text="This is a blog post header"></BlogCard>
+                  </Grid>
+                </Grid>
+              </Grid>
 
-                  <Divider style={{marginTop: "40px"}}></Divider>
-              </div>
-            </div>
+              <Grid item>
+                <Button variant="outlined" style={{outline: "none", background: "#1D3557", color: "white"}}>View More</Button>
+              </Grid>
+            </Grid> {/** news */}
 
-            <div class="wrapper">
-              <div id="tools" style={{display: "flex", width:"70%", margin: "0 auto", paddingTop: "50px", flexDirection: "column"}}>
-                  <div>
-                      <Typography variant="h3" align="left" style={{fontWeight: "bold"}}> Tools </Typography>
-                      <Typography variant="body1" align="left" style={{fontWeight: "500", paddingTop: "1%"}}>
-                        Our tools help researchers to implement their algorithms related to motion planning for automated vehicles.
-                      </Typography>
-                      <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly", marginTop: "20px"}}>
-                        <BlogCard text="This is a blog post header"></BlogCard>
-                        <BlogCard text="This is a blog post header"></BlogCard>
-                        <BlogCard text="This is a blog post header"></BlogCard>
-                        <BlogCard text="This is a blog post header"></BlogCard>
-                      </div>
+            <Divider style={{marginTop: "40px"}}></Divider>
 
-                      <div style={{textAlign: "center", marginTop: "20px"}}>
-                        <Button variant="outlined" style={{outline: "none", background: "#1D3557", color: "white"}}>View More</Button>
-                      </div>
+            <Grid container spacing={2} direction="column" justify="center" alignItems="center" style={{width: "75%", margin: "0 auto"}}>
+              <Grid item xs={12}>
+                <Typography variant="h3" style={{fontWeight: "bold"}}>
+                  Forum
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1" align="center" style={{fontWeight: "500"}}>
+                  CommonRoad has also a welcoming and open community and our ecosystem grows larger day by day.
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Button variant="outlined" style={{outline: "none", background: "#1D3557", color: "white"}}>View More</Button>
+              </Grid>
+            </Grid>
 
-
-                      <Divider style={{marginTop: "40px"}}></Divider>
-                  </div>
-                </div>
-              </div>
-
-            <div class="wrapper">
-              <div id="news" style={{display: "flex", width:"70%", margin: "0 auto", paddingTop: "50px", flexDirection: "column"}}>
-                  <div>
-                      <Typography variant="h3" align="left" style={{fontWeight: "bold"}}> News</Typography>
-                      <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly", marginTop: "20px"}}>
-                        <BlogCard text="This is a blog post header"></BlogCard>
-                        <BlogCard text="This is a blog post header"></BlogCard>
-                        <BlogCard text="This is a blog post header"></BlogCard>
-                        <BlogCard text="This is a blog post header"></BlogCard>
-                      </div>
-
-                      <div style={{textAlign: "center", marginTop: "20px"}}>
-                        <Button variant="outlined" style={{outline: "none", background: "#1D3557", color: "white"}}>View More</Button>
-                      </div>
-
-
-                      <Divider style={{marginTop: "40px"}}></Divider>
-                  </div>
-                </div>
-              </div>
-
-              <div class="wrapper">
-                <div id="blog-and-forum" style={{display: "flex", width:"70%", margin: "0 auto", paddingTop: "50px", flexDirection: "column"}}>
-                  <div style={{marginTop: "50px"}}>
-                      <Typography variant="h3" align="left" style={{fontWeight: "bold"}}> Forum </Typography>
-                      <Typography variant="body1" align="center" style={{fontWeight: "500"}}>CommonRoad has also a welcoming and open community and our ecosystem grows larger day by day.</Typography>
-                      <div style={{textAlign: "center", marginTop: "20px"}}>
-                        <Button variant="outlined" style={{outline: "none", background: "#1D3557", color: "white"}}>Go to Forum</Button>
-                      </div>
-
-                      <Divider style={{marginTop: "40px"}}></Divider>
-                  </div>
-                </div>
-
-            </div>
+            <Divider style={{marginTop: "40px"}}></Divider>
         </div>
     );
   }
